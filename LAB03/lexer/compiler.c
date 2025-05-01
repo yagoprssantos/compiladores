0c1152788b502949fac399387c2d8fd07d6274cc
yagoprssantos <yagprogramr@gmail.com>
2025-05-01 01:26:23 -0300

    aw

--- LAB03/lexer/compiler.c
+++ LAB03/lexer/compiler.c
@@ -10,43 +10,48 @@ struct lex_process_functions compiler_lex_functions = {
 /* BEGIN - LAB 3 ----------------------------------*/
 void compiler_error(struct compile_process *compiler, const char *msg, ...)
 {
-  va_list args;
-  va_start(args, msg);
-  vfprintf(stderr, msg, args);
-  fprintf(stderr, " na linha %i, coluna %i, arquivo %s\n", compiler->pos.line, compiler->pos.col, compiler->pos.filename);
+    va_list args;
+    va_start(args, msg);
+    vfprintf(stderr, msg, args);
+    fprintf(stderr, " na linha %i, coluna %i, arquivo %s\n", compiler->pos.line, compiler->pos.col, compiler->pos.filename);
 
-  exit(-1);
+    exit(-1);
 }
 
 void compiler_warning(struct compile_process *compiler, const char *msg, ...)
 {
-  va_list args;
-  va_start(args, msg);
-  vfprintf(stderr, msg, args);
-  fprintf(stderr, " na linha %i, coluna %i, arquivo %s\n", compiler->pos.line, compiler->pos.col, compiler->pos.filename);
+    va_list args;
+    va_start(args, msg);
+    vfprintf(stderr, msg, args);
+    fprintf(stderr, " na linha %i, coluna %i, arquivo %s\n", compiler->pos.line, compiler->pos.col, compiler->pos.filename);
 
-  exit(-1);
+    exit(-1);
 }
 
 int compile_file(const char *filename, const char *out_finename, int flags)
 {
-  struct compile_process *process = compile_process_create(filename, out_finename, flags);
-  if (!process)
-  {
-    return COMPILER_FAILED_WITH_ERRORS;
-  }
+    struct compile_process *process = compile_process_create(filename, out_finename, flags);
+    if (!process)
+    {
+        return COMPILER_FAILED_WITH_ERRORS;
+    }
 
-  /* Aqui entra a analise lexica*/
-  struct lex_process *lex_process = lex_process_create(process, &compiler_lex_functions, NULL);
+    /* Aqui entra a analise lexica*/
+    struct lex_process *lex_process = lex_process_create(process, &compiler_lex_functions, NULL);
 
-  if (!lex_process)
-    return COMPILER_FAILED_WITH_ERRORS;
+    if (!lex_process)
+        return COMPILER_FAILED_WITH_ERRORS;
 
-  if (lex(lex_process) != LEXICAL_ANALYSIS_ALL_OK)
-    return COMPILER_FAILED_WITH_ERRORS;
-  /* Aqui entra o parsing do codigo*/
+    if (lex(lex_process) != LEXICAL_ANALYSIS_ALL_OK)
+        return COMPILER_FAILED_WITH_ERRORS;
 
-  /* Aqui entra a geracao de codigo*/
+    process->token_vec = lex_process->token_vec; /* LAB3 */
 
-  return COMPILER_FILE_COMPILED_OK;
+    /* Aqui entra o parsing do codigo*/
+    if (parse(process) != PARSE_ALL_OK)
+        return COMPILER_FAILED_WITH_ERRORS; /* LAB3 */
+
+    /* Aqui entra a geracao de codigo*/
+
+    return COMPILER_FILE_COMPILED_OK;
 }
--- LAB03/lexer/cprocess.c
+++ LAB03/lexer/cprocess.c
@@ -1,23 +1,27 @@
 #include "compiler.h"
 
-struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags){
+struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags)
+{
   FILE *file = fopen(filename, "r");
-  
-  if (!file) {
+
+  if (!file)
+  {
     return NULL;
   }
 
-  FILE* out_file = NULL;
-  if (filename_out) {
+  FILE *out_file = NULL;
+  if (filename_out)
+  {
     out_file = fopen(filename_out, "w");
-    if (!out_file) {
+    if (!out_file)
+    {
       return NULL;
     }
   }
 
-  struct compile_process* process = calloc(1, sizeof(struct compile_process));
-  process->node_vec = vector_create(sizeof(struct node*)); 
-  process->node_tree_vec = vector_create(sizeof(struct node*));
+  struct compile_process *process = calloc(1, sizeof(struct compile_process));
+  process->node_vec = vector_create(sizeof(struct node *));
+  process->node_tree_vec = vector_create(sizeof(struct node *));
   process->flags = flags;
   process->cfile.fp = file;
   process->ofile = out_file;
@@ -27,21 +31,24 @@ struct compile_process* compile_process_create(const char* filename, const char*
 
 /* BEGIN - LAB 2 -----------------------------------------*/
 // Pega um caractere do arquivo e atualiza a posição
-char compile_process_next_char(struct lex_process* lex_process) {
-  struct compile_process* compiler = lex_process->compiler;
-  compiler->pos.col += 1;        // Atualiza a posição da coluna
+char compile_process_next_char(struct lex_process *lex_process)
+{
+  struct compile_process *compiler = lex_process->compiler;
+  compiler->pos.col += 1; // Atualiza a posição da coluna
   char c = getc(compiler->cfile.fp);
-  if (c == '\n') {
-      compiler->pos.line += 1;
-      compiler->pos.col = 1;
+  if (c == '\n')
+  {
+    compiler->pos.line += 1;
+    compiler->pos.col = 1;
   }
 
   return c;
 }
 
 // Apenas pega um caractere do arquivo
-char compile_process_peek_char(struct lex_process* lex_process) {
-  struct compile_process* compiler = lex_process->compiler;
+char compile_process_peek_char(struct lex_process *lex_process)
+{
+  struct compile_process *compiler = lex_process->compiler;
   char c = getc(compiler->cfile.fp);
   ungetc(c, compiler->cfile.fp);
 
@@ -49,9 +56,9 @@ char compile_process_peek_char(struct lex_process* lex_process) {
 }
 
 // Adicionar um caractere no arquivo.
-void compile_process_push_char(struct lex_process* lex_process, char c) {
-  struct compile_process* compiler = lex_process->compiler;
+void compile_process_push_char(struct lex_process *lex_process, char c)
+{
+  struct compile_process *compiler = lex_process->compiler;
   ungetc(c, compiler->cfile.fp);
-  
 }
 /* END - LAB 2 -------------------------------------------*/
