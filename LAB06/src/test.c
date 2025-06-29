c0911fb8146bac3325f91fbffe87feb7a48e805a
yagoprssantos <yagprogramr@gmail.com>
2025-06-29 17:19:41 -0300

    LAB06 - Documentação atualizada

--- LAB06/src/test.c
+++ LAB06/src/test.c
@@ -1,16 +1,16 @@
-// Vetores
-int A[50][50];
-float B[100];
+// Variáveis
+A igual int;
+matriz igual float[5][5];
 
 // Structs
-struct ABC1
-{
-  int A
-  float B
-  double C
-};
+struct ABC / int A\ / int B\ / int C\;
+struct Pessoa / int idade\ / float altura\ / char nome[50]\;
+struct Complexo / int *ponteiro\ / float matriz[3][3]\;
 
-struct ABC2
-{
-  int A, B, C, D, E
-};
+// If/Else
+if c
+  ? return 0 ? return 1;
+if x
+  == y ? x = x + 1 ? x = x - 1;
+if a
+  > b ? a = a + 1 ? a = a - 1;
--- LAB06/src/test_struct_nova_sintaxe.c
+++
@@ -1,7 +0,0 @@
-struct ABC /int A\;
-struct Pessoa /int idade\ /float altura\ /char nome[50]\;
-struct Complexo /int* ponteiro\ /float matriz[3][3]\;
-
-
-
-
